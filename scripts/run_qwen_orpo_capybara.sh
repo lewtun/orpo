d34475b4273a37dfd23a24d0961774a69dd7bd1f
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2024-03-30 08:55:32 +0000

    Up

--- scripts/run_qwen_orpo_capybara.sh
+++ scripts/run_qwen_orpo_capybara.sh
@@ -25,6 +25,24 @@ accelerate launch --config_file ./src/accelerate/fsdp.yaml main.py \
     --save_dir data/Qwen1.5-0.5B-capybara-dpo-7k-v0.1 \
     --hub_model_id orpo-explorers/Qwen1.5-0.5B-capybara-dpo-7k-v0.1
     
+accelerate launch --config_file ./src/accelerate/ds3.yaml main.py \
+    --lr 5e-6 \
+    --torch_compile False \
+    --alpha 0.05 \
+    --lr_scheduler_type inverse_sqrt \
+    --warmup_steps 100 \
+    --model_name Qwen/Qwen1.5-0.5B \
+    --data_name argilla/distilabel-capybara-dpo-7k-binarized \
+    --num_train_epochs 3 \
+    --optim adamw_torch \
+    --gradient_accumulation_steps 1 \
+    --prompt_max_length 1792 \
+    --response_max_length 2048 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 8 \
+    --num_proc 32 \
+    --flash_attention_2 \
+    --push_to_hub false
 
 # 72B - can't fit on 1 node :(
 
